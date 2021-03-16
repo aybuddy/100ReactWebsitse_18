a8a17ff7873a537823010d6058e2e52b3c41f536
Aaron Yu <ayybuddyy@gmail.com>
2021-03-16 11:57:35 -0400

    Style the app component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import './App.css';
 function App() {
   return (
     <div className='app'>
-      <h1>Instagram Like Clone</h1>
+      <div className='app__top'>
+        <img
+          src='https://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c521.png'
+          alt=''
+          className='app__logo'
+        />
+        <h1>Reels</h1>
+        {/* image at top - logo  */}
+        {/* reels text  */}
+      </div>
+
+      <div className='app__videos'>
+        {/* container of app videos (scrollable) */}
+        {/* <Video /> */}
+        {/* <Video /> */}
+        {/* <Video /> */}
+        {/* <Video /> */}
+      </div>
     </div>
   );
 }
